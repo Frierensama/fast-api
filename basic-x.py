14fed39a31b7c0bbb4a41c1f949f45eb891ca8c3
code:/02 <rudeusgreyrat.zeta002@gmail.com>
2026-08-16 12:30:10 +0530

    Update basic-x.py

--- basic-x.py
+++ basic-x.py
@@ -3,31 +3,28 @@ import json
 
 app = FastAPI()
 
+@app.get('/')
+def home():
+    return {'message':'root address endpoint'}
+
 def load_data():
     with open('basic-x.json','r') as f:
         data = json.load(f)
-
     return data
 
-
-@app.get('/')
-def hello():
-    return {'message':'whatever'}
-
-# path param
 @app.get('/character/{path_param}')
-def character(path_param:str = Path(...,description='character name',example='rudy')):
+def view_character(path_param:str = Path(...,description='name of a character',examples=['rudy','sylphy'])):
     data = load_data()
-    if path_param in data:
-        return data[path_param]
-    raise HTTPException(status_code=404,detail=f"no character named {path_param}")
+    if path_param not in data:
+        raise HTTPException(status_code=404,detail='no such name endpoint')
+    return data[path_param]
 
-# query param
-@app.get('/view')
-def viewall(sort_param:str = Query('asc',description='desc or asc - sorting param')):
+@app.get('/viewdata')
+def view_data(wannasort:str = Query('asc',description='an element to sort')):
     data = load_data()
-    if sort_param not in ['asc','desc']:
-        raise HTTPException(status_code=400,detail='wrong sort param. use asc or desc')
-    
-    data = sorted( data.values(), key= lambda x : x.get('age'), reverse= True if sort_param == 'desc' else False)
-    return data
+
+    if wannasort not in ['asc','desc']:
+        raise HTTPException(status_code=400)
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get('age'),reverse =True if wannasort == 'desc' else False)
+    return sorted_data
